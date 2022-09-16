bdae042454dc954744eac814f12ea87b942955f4
nadeemanjana <nadeema.s2@gmail.com>
2022-09-16 16:43:26 +0100

    testing

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('ADDR_for_website.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.port||3000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
 server.listen(port);
